c8ff49b05511d61b9d1e4b394811fe116920f6e1
iRay888 <80120542+iRay888@users.noreply.github.com>
2025-12-24 18:56:30 -0500

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,38 +1,37 @@
 -- https://discord.gg/tUEJZYvF9d
--- known to work on wave, nihon, awp.gg, syn z, seliware
--- known to work on wave, nihon, awp.gg, syn z, seliware, sirhurt
-local isLimited = ...
-local environment = identifyexecutor and identifyexecutor() or ""
-local source = game:HttpGet("https://raw.githubusercontent.com/iRay888/wapus/refs/heads/main/" .. (isLimited and "source-limited.lua" or "source.lua"))
-if (string.find(string.lower(environment), "wave") or string.find(string.lower(environment), "choco")) and not executed then
-    run_on_actor(get_deleted_actors()[1], source)
-elseif getfflag and string.find(string.lower(tostring(getfflag("DebugRunParallelLuaOnMainThread"))), "true") and not executed then
-    loadstring(source)()
-elseif string.find(environment, "AWP") ~= nil and not executed then
-    for _, v in getactors() do
-        run_on_actor(v, [[
-            for _, func in getgc(false) do
-                if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
-                    ]] .. source .. [[
-                    break
-                end
-            end
-        ]])
+local executor = string.lower(identifyexecutor and identifyexecutor() or "")
+local source = game:HttpGet("https://raw.githubusercontent.com/iRay888/wapus/refs/heads/main/source.lua"))
+local threadSource = [[
+    for _, func in getgc(false) do
+        if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
+            ]] .. source .. [[
+            break
+        end
     end
-elseif (string.find(string.lower(environment), "zenith") or string.find(string.lower(environment), "volt") or string.find(string.lower(environment), "potassium")) and not executed then
-    for _, actor in getactorthreads() do
-        run_on_thread(actor, [[
-            for _, func in getgc(false) do
-                if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
-                    ]] .. source .. [[
-                    break
-                end
-            end
-        ]])
+]]
+
+local function runSource(runner, getAll)
+    for _, actor in getAll() do
+        runner(actor, threadSource)
     end
-else
-    queue_on_teleport(game:HttpGet("https://raw.githubusercontent.com/iRay888/wapus/refs/heads/main/hook.lua") .. "task.wait(5);" .. source)
-    setfflag("DebugRunParallelLuaOnMainThread", "True")
-    game:GetService("TeleportService"):TeleportToPlaceInstance(game.PlaceId, game.JobId)
 end
+
+if (string.find(executor, "wave") or string.find(executor, "choco")) and (not getgenv().executed) then
+    runSource(run_on_actor, get_deleted_actors)
+elseif string.find(executor, "volt") and (not getgenv().executed) then
+    runSource(run_on_actor, getactors)
+elseif string.find(executor, "potassium") and (not getgenv().executed) then
+    runSource(run_on_thread, getactorthreads)
+elseif getfflag and (string.lower(tostring(getfflag("DebugRunParallelLuaOnMainThread"))) == "true") then
+    loadstring(source)()
+elseif setfflag then
+    setfflag("DebugRunParallelLuaOnMainThread", "True")
+
+    if queue_on_teleport then
+        queue_on_teleport(source)
+    end
+    
+    game:GetService("TeleportService"):Teleport(game.PlaceId)
+end
+
 getgenv().executed = true
